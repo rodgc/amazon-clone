14092eb416a5734e8de17dd5e34797a6c4f033a7
rodgc <rod.gc@hotmail.com>
2020-09-09 11:48:06 -0500

    Add HeaderContainer

---
+++ src/Header/HeaderContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderDisplay from './HeaderDisplay'
+
+function HeaderContainer() {
+    return (
+        <HeaderDisplay />
+    )
+}
+
+export default HeaderContainer
