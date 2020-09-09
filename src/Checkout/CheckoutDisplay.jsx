1c73755d85eccdf7bff76ae5a2cadfed654f11af
rodgc <rod.gc@hotmail.com>
2020-09-09 14:39:43 -0500

    Add Subtotal container

--- src/Checkout/CheckoutDisplay.jsx
+++ src/Checkout/CheckoutDisplay.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import { Subtotal } from "../Subtotal";
 
 function CheckoutDisplay() {
   return (
@@ -22,7 +23,9 @@ function CheckoutDisplay() {
         </div>
       </div>
 
-      <div className="checkout__right"></div>
+      <div className="checkout__right">
+          <Subtotal />
+      </div>
     </div>
   );
 }
---
+++ src/Subtotal/SubtotalContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SubtotalDisplay from './SubtotalDisplay'
+
+function SubtotalContainer() {
+    return (
+        <SubtotalDisplay />
+    )
+}
+
+export default SubtotalContainer
