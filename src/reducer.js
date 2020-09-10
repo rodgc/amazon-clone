59c3a37116760ce623f334f12ce48af826b1c134
rodgc <rod.gc@hotmail.com>
2020-09-10 09:51:19 -0500

    Fix Typos on reducer

--- src/reducer.js
+++ src/reducer.js
@@ -11,30 +11,37 @@ export const getBasketTotal = (basket) =>
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD_TO_BASKET:
-      return { ...state, basket: [...state.basket, action.item] };
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
     case REMOVE_FROM_BASKET:
       const index = state.basket.findIndex(
-        (baskedItem) => baskedItem.id === action.id
+        (basketItem) => basketItem.id === action.id
       );
       let newBasket = [...state.basket];
 
       if (index >= 0) {
         newBasket.splice(index, 1);
+
       } else {
         console.warn(
-          `Cant remove prodcut (id: ${action.id}) as its not in basket!`
-        );
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        )
       }
 
       return {
         ...state,
-        basket: newBasket,
-      };
+        basket: newBasket
+      }
+    
     case SET_USER:
       return {
         ...state,
-        user: action.user,
-      };
+        user: action.user
+      }
+
     default:
       return state;
   }
