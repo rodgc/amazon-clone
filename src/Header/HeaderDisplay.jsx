4b79ffa82ff220081fe9198893126202873396d4
rodgc <rod.gc@hotmail.com>
2020-09-11 09:21:14 -0500

    Add Route to Orders page

--- src/Header/HeaderDisplay.jsx
+++ src/Header/HeaderDisplay.jsx
@@ -19,7 +19,7 @@ function HeaderDisplay({ basket, user, handleAuthentication }) {
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <Link to={user ? '' : "/login"}>
+        <Link to={user ? "" : "/login"}>
           <div className="header__option" onClick={handleAuthentication}>
             <span className="header__optionLineOne">
               Hello {user ? user?.email : "Guest"}
@@ -29,10 +29,12 @@ function HeaderDisplay({ basket, user, handleAuthentication }) {
             </span>
           </div>
         </Link>
-        <div className="header__option">
-          <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineTwo">& Orders</span>
-        </div>
+        <Link to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
